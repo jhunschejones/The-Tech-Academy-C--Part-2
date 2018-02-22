03301ad8c8d51498759f8f3da9226c586cf95948
Joshua Jones <joshjones103@gmail.com>
2018-02-21 19:55:23 -0800

    Initial commit, superheros MVC app

---
+++ MVCFirstChallenge/FirstChallenge/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstChallenge.Models;
+
+namespace FirstChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var comics = ComicBookManager.GetComicBooks();
+            return View(comics);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var comics = ComicBookManager.GetComicBooks();
+            // using this to find the comic with the id passed in
+            // the => is a LINQ statement
+            var returnComic = comics.FirstOrDefault(p => p.ComicBookId == id);
+            return View(returnComic);
+        }
+    }
+}
